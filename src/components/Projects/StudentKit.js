92a5ff5ae223ee21b3352494f9183f68310a2233
candicechu0126 <candicechu126@gmail.com>
2022-09-15 22:06:43 +0800

    updated

--- src/components/Projects/StudentKit.js
+++ src/components/Projects/StudentKit.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 import titleImg from "../../Assets/Projects/title/title4.png";
 
 const Title = styled.div`
